93402436ec145231504a62f24e371810b285ab4d
Pablicco <pabesqamo@alu.edu.gva.es>
2025-01-08 13:28:38 +0100

    Clase 8/1/2025

---
+++ phyton-5/ejercicio-1-phyton-5.py
@@ -0,0 +1,8 @@
+n = int(input("Dame un número, te diré si es positivo, negativo o 0: "))
+
+if n > 0 :
+    print("Tu número es positivo")
+elif n < 0 :
+    print("Tu número es negativo")
+else :
+    print("Tu número es 0")
